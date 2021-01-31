614c901198da9c125ccfbf8f12f285e5d88d483a
mariusaee <mariusae@me.com>
2021-01-31 21:17:50 +0300

    All code is done! Great! It's worked!

--- rgbColorMixer/ViewController.swift
+++ rgbColorMixer/ViewController.swift
@@ -8,31 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var coloredView: UIView!
-    
+    // Labels
     @IBOutlet var redLabel: UILabel!
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
     
+    
+    // Sliders
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var coloredView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        coloredView.layer.cornerRadius = 20
+        
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
     @IBAction func redSliderAction() {
-        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
     @IBAction func greenSliderAction() {
-        
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-    
+
     @IBAction func blueSliderAction() {
-        
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-    
 }
+
 
