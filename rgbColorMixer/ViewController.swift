7736ed0d9a73f55b04d9b2fee2defaa634c3b2b5
mariusaee <mariusae@me.com>
2021-01-31 13:29:54 +0300

    Initial Commit

---
+++ rgbColorMixer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgbColorMixer
+//
+//  Created by Marius Malyshev on 31.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
